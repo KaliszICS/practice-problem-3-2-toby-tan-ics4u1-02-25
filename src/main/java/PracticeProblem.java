9367ac610f955689df3d47e9302bd4db890f365b
toby-tan-ics4u1-02-25 <toby.tan@student.tdsb.on.ca>
2025-04-09 00:24:23 +0000

    KAKABAM

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,16 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println(towerOfHanoi(6));
 	}
+	 public static int towerOfHanoi(int num) {
+        if (num < 3) {
+            return -1;
+        }
+        if (num == 1){
+			return 1;
+		}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+		return (int) Math.pow(2, num) - 1;
+    }
 }
